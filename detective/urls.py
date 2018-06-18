4079bb6a63996fed9ad18e765395618c86f8b7cc
Erik Telepovský <erik.telepovsky@gmail.com>
2018-06-18 12:04:05 +0200

    app_name in urls.py

--- detective/urls.py
+++ detective/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from detective.views import TrackingLogDetailView
 
+app_name = 'detective'
 
 urlpatterns = [
     url(r'^trackinglog/detail/(?P<pk>[-\d]+)/$', TrackingLogDetailView.as_view(), name='trackinglog_detail'),
